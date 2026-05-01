2c92e4b6184d757263e658d4c03a4c873e00dad6
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2026-05-01 22:35:18 +0200

    reset styles, update favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div></div>;
-};
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <main className="container">
+        <div className="content-wrapper">hello world</div>
+      </main>
+    </>
+  );
+}
 
 export default App;
